7405e4ca4282aa6e0f0902d18890ea496af89da0
Erick Dantas <ericksdantas144@gmail.com>
2021-11-19 17:15:32 -0300

    validate

--- src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.nelioalves.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.nelioalves.cursomc.domain.Categoria;
 
 
@@ -9,8 +13,11 @@ import com.nelioalves.cursomc.domain.Categoria;
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-
+	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome; 
 
 	
